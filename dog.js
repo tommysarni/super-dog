f0e985cd3ecce4c8c88167dad3a5ff93391a531f
Tommy Sarni <tsarn1799@gmail.com>
2024-12-21 12:46:07 -0500

    added modal/hamburger menu fucntionality

--- dog.js
+++ dog.js
@@ -313,12 +313,44 @@ function Dog(id) {
   };
 
   this.addButtonFuntionality = () => {
+
+    const removeModal = (event) => {
+      const modalEl = document.querySelector('body>.modal');
+      const shouldClose = (target) => {
+        if (!target) return false;
+        while (target) {
+          if (target === modalEl) return false;
+          target = target.parentElement;
+        }
+        return true;
+      };
+      if (modalEl && shouldClose(event.target)) {
+        modalEl.style.top = '-55vh';
+      }
+    };
+
     const menuBtn = document.querySelector('button.hamburger');
     if (menuBtn) {
       menuBtn.addEventListener('click', () => {
-        console.log('clicked menu');
+        const modalEl = document.querySelector('body>.modal');
+        if (modalEl) {
+          modalEl.style.top = '0';
+          document.addEventListener("click", removeModal, true);
+        }
       });
     }
+
+    const modalEscape = document.querySelector('.modal>button.modalEscape');
+    if (modalEscape) {
+      modalEscape.addEventListener('click', (event) => {
+        const modalEl = document.querySelector('body>.modal');
+        if (modalEl) {
+          modalEl.style.top = '-55vh';
+          document.removeEventListener("click", removeModal);
+        }
+      });
+    }
+
     const unitsBtn = document.querySelector('button.units');
     if (unitsBtn) {
       unitsBtn.addEventListener('click', (e) => {
